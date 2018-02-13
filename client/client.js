01d133eb0c8856aa03b4d9ca334870b0e0468248
Alexander Green <aeg0322@gmail.com>
2018-02-13 11:53:15 -0500

    save

--- client/client.js
+++ client/client.js
@@ -3,7 +3,7 @@ let client = new Client.Client()
 
 let server_ip = 'localhost'
 
-client.get('172.22.241.70:8080', (data, response) => {
+client.get('localhost:8080', (data, response) => {
   console.log(data)
   console.log(response)
 })
