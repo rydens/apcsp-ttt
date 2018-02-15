69a8865540b1e6663e37ffee63d3deab0a4ed404
Ryan Densmore <rdensmore365@gmail.com>
2018-02-15 11:22:25 -0500

    save

--- client/client.js
+++ client/client.js
@@ -1,18 +1,29 @@
 const net = require('net')
+const rl = require('readline-sync') 
 
-const server = '127.0.0.1'
-const port = '8080'
+let serverIP
+let serverPort
 
-let client = new net.Socket()
-client.connect(port, server, () => {
-  console.log(`Connected to: ${server}:${port}`)
-})
-client.write('Hello!')
+// Colors for POSIX-compliant terminals
+let green = '\033[0;32m'
+let cyan = ''
+let red = ''
+let boldGreen = ''
+let boldCyan = ''
+let 
 
-client.on('data', (data) => {
-  console.log(`Data: ${data}`)
-})
+let getServer = () => {
+  console.log(`Please type your server.`)
+  console.log(`<ip>:<port>`)
+  let server = rl.question('> ')
+  serverIP = server.split(':')[0]
+  serverPort = server.split(':')[1]
+  console.log(`Connecting to ${serverIP}:${serverPort}...`)
+}
+let sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 
-client.on('close', () => {
-  console.log('Connection Closed')
-})
+console.log(`Welcome to TIC TAC TOE!`)
+getServer()
+console.log('\033[0;32mConnected!')
+
+
