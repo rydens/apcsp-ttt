487de876764ec3cc0c2550ec7e5b174f9bd1772b
Alexander Green <aeg0322@gmail.com>
2018-02-15 09:46:57 -0500

    save

--- server/main.go
+++ server/main.go
@@ -16,6 +16,20 @@ func get_json(url string, target interface{}) error {
 	defer r.Body.Close()
 	return json.NewDecoder(r.Body).Decode(target)
 }
+func create_board() {
+	type Board struct {
+		1	int	`json:"1"`
+		2	int	`json:"2"`
+		3	int	`json:"3"`
+		4	int	`json:"4"`
+		5	int	`json:"5"`
+		6	int	`json:"6"`
+		7	int	`json:"7"`
+		8	int	`json:"8"`
+		9	int	`json:"9"`
+	}
+	
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
