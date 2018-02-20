db7e218d614de1539ef5a96f2bde2d744f84f7a5
Alexander Green <aeg0322@gmail.com>
2018-02-20 12:42:17 -0500

    save

--- server/tcp-server.go
+++ server/tcp-server.go
@@ -2,29 +2,35 @@ package main
 
 import "net"
 import "fmt"
+
 import "bufio"
-import "strings" // only needed below for sample processing
 
+//import "strings" // only needed below for sample processing
+
+//https://systembash.com/a-simple-go-tcp-server-and-tcp-client/
 func main() {
-
-	fmt.Println("Launching server...")
-
-	// listen on all interfaces
-	ln, _ := net.Listen("tcp", ":8080")
-
-	// accept connection on port
-	conn, _ := ln.Accept()
-
-	// run loop forever (or until ctrl-c)
+	type Board struct {
+		One   int
+		Two   int
+		Three int
+		Four  int
+		Five  int
+		Six   int
+		Seven int
+		Eight int
+		Nine  int
+	}
 	for {
-		// will listen for message to process ending in newline (\n)
+		fmt.Println("Launching server...")
+
+		ln, _ := net.Listen("tcp", ":8080")
+
+		conn, _ := ln.Accept()
+
 		message, _ := bufio.NewReader(conn).ReadString('\n')
-		// output message received
-		fmt.Print("Message Received:", string(message))
-		// sample process for string received
-		newmessage := strings.ToUpper(message)
-		// send new string back to client
-		conn.Write([]byte(newmessage + "\n"))
+		//	fmt.Print("Message Received:", string(message))
+		//	newmessage := strings.ToUpper(message)
+		conn.Write([]byte(message + "\n"))
 	}
 
 }
